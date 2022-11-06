7ea19938afaabe563df9eb8324aa0b44c4e28816
carefree0910 <syameimaru.saki@gmail.com>
2022-11-06 10:22:15 +0800

    ⬆️Upgraded `carefree-learn` dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(exclude=("tests",)),
     install_requires=[
         "carefree-client>=0.1.5",
-        "carefree-learn[cv]>=0.3.3.13",
+        "carefree-learn[cv]>=0.3.3.14",
     ],
     extras_require={
         "kafka": [
