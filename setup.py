56ce3a431c879693dcff14d9d6c9909a06244ccf
carefree0910 <syameimaru.saki@gmail.com>
2023-01-12 16:23:58 +0800

    ⬆️Upgraded `carefree-client` dependency

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         "click>=8.1.3",
         "fastapi==0.88.0",
-        "carefree-client>=0.1.6",
+        "carefree-client>=0.1.7",
         "carefree-learn[cv]>=0.3.3.31",
     ],
     extras_require={
