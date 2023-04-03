c47fac80ccb0c03cf83e2cff4b8e9d27bb139706
carefree0910 <syameimaru.saki@gmail.com>
2023-04-03 16:00:37 +0800

    🐛Fixed migration issues

--- cfcreator/common.py
+++ cfcreator/common.py
@@ -129,7 +129,7 @@ def init_sd() -> ControlledDiffusionAPI:
                 print(f">> converting {version}")
                 model_path = os.path.join(external_dir, f"{version}.ckpt")
                 d = cflearn.scripts.sd.convert(model_path, m, load=False)
-                m.sd_weights[f"ldm_sd_{version}"] = d
+                m.sd_weights[version] = d
             print("> prepare ControlNet weights")
             m.prepare_control_defaults()
             print("> prepare ControlNet Annotators")
