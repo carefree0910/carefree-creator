52936de96743765a397ad8b93627da8383a3843c
carefree0910 <syameimaru.saki@gmail.com>
2023-04-04 13:22:39 +0800

    ✨Implemented `push_direct`

--- cfcreator/sdks/direct.py
+++ cfcreator/sdks/direct.py
@@ -1,6 +1,8 @@
 # `direct` sdk is used to access to the server launched by `cfcreator serve`
 
 from cfcreator import *
+from typing import Any
+from typing import Dict
 from PIL.Image import Image
 
 from .utils import *
@@ -8,6 +10,10 @@ from .utils import *
 
 async def txt2img(model: Txt2ImgSDModel) -> Image:
     return await get_image_res(get_url(txt2img_sd_endpoint), model)
+
+
+async def push_direct(endpoint: str, params: Dict[str, Any]) -> Image:
+    return await get_image_res(get_url(endpoint), params)
 
 
 __all__ = [
