8894ae777e63d26cf2dc3dabed0a3dc508e316e3
carefree0910 <syameimaru.saki@gmail.com>
2023-06-07 11:02:52 +0800

    🎨Extracted `workflow`

--- cfcreator/__init__.py
+++ cfcreator/__init__.py
@@ -2,3 +2,4 @@ from .cos import *
 from .common import *
 from .endpoints import *
 from .parameters import *
+from .workflow import *
---
+++ cfcreator/workflow.py
@@ -0,0 +1,211 @@
+import io
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from PIL import Image
+from typing import Any
+from typing import Set
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Optional
+from typing import NamedTuple
+from fastapi import Response
+from pydantic import Field
+from pydantic import BaseModel
+from cftool.data_structures import Item
+from cftool.data_structures import Bundle
+
+from .common import IAlgorithm
+from .common import ReturnArraysModel
+
+
+# data structures
+
+
+class InjectionPack(BaseModel):
+    index: int
+    field: str
+
+
+class WorkNode(BaseModel):
+    key: str = Field(
+        ...,
+        description="Key of the node, should be identical within the same workflow",
+    )
+    endpoint: str = Field(..., description="Algorithm endpoint of the node")
+    injections: Dict[str, InjectionPack] = Field(
+        ...,
+        description=(
+            "Injection map, maps 'key' from other `WorkNode` (A) to 'index' of A's results  & "
+            "'field' of the algorithm's field. In runtime, we'll collect "
+            "the (list of) results from the depedencies (other `WorkNode`) and "
+            "inject the specific result (based on 'index') to the algorithm's field.\n"
+            "> If external caches is provided, the 'key' could be the key of the external cache.\n"
+            "> Hierarchy injection is also supported, you just need to set 'field' to:\n"
+            ">> `a.b.c` to inject the result to data['a']['b']['c']\n"
+            ">> `a.0.b` to inject the first result to data['a'][0]['b']\n"
+        ),
+    )
+    data: Dict[str, Any] = Field(..., description="Algorithm's data")
+
+    def to_item(self) -> Item["WorkNode"]:
+        return Item(self.key, self)
+
+
+class ToposortResult(NamedTuple):
+    in_edges: Dict[str, Set[str]]
+    hierarchy: List[List[Item[WorkNode]]]
+    edge_labels: Dict[Tuple[str, str], str]
+
+
+class Workflow(Bundle[WorkNode]):
+    def push(self, node: WorkNode) -> None:
+        return super().push(node.to_item())
+
+    def toposort(self) -> ToposortResult:
+        in_edges = {item.key: set() for item in self}
+        out_degrees = {item.key: 0 for item in self}
+        edge_labels: Dict[Tuple[str, str], str] = {}
+        for item in self:
+            for dep, pack in item.data.injections.items():
+                in_edges[dep].add(item.key)
+                out_degrees[item.key] += 1
+                edge_labels[(item.key, dep)] = pack.field
+
+        ready = [k for k, v in out_degrees.items() if v == 0]
+        result = []
+        while ready:
+            layer = ready.copy()
+            result.append(layer)
+            ready.clear()
+            for dep in layer:
+                for node in in_edges[dep]:
+                    out_degrees[node] -= 1
+                    if out_degrees[node] == 0:
+                        ready.append(node)
+
+        if len(self) != sum(map(len, result)):
+            raise ValueError("cyclic dependency detected")
+
+        hierarchy = [list(map(self.get, layer)) for layer in result]
+        return ToposortResult(in_edges, hierarchy, edge_labels)
+
+    def get_dependency_path(self, target: str) -> ToposortResult:
+        def dfs(key: str) -> None:
+            if key in reachable:
+                return
+            reachable.add(key)
+            for dep_key in self.get(key).data.injections:
+                dfs(dep_key)
+
+        reachable = set()
+        dfs(target)
+        in_edges, raw_hierarchy, edge_labels = self.toposort()
+        hierarchy = []
+        for raw_layer in raw_hierarchy:
+            layer = []
+            for item in raw_layer:
+                if item.key in reachable:
+                    layer.append(item)
+            if layer:
+                hierarchy.append(layer)
+        return ToposortResult(in_edges, hierarchy, edge_labels)
+
+    def render(
+        self,
+        *,
+        target: Optional[str] = None,
+        fig_w_ratio: int = 4,
+        fig_h_ratio: int = 3,
+        dpi: int = 200,
+        node_size: int = 2000,
+        node_shape: str = "s",
+        node_color: str = "lightblue",
+        layout: str = "multipartite_layout",
+    ) -> Image.Image:
+        # setup graph
+        G = nx.DiGraph()
+        if target is None:
+            target = self.last.key
+        in_edges, hierarchy, edge_labels = self.get_dependency_path(target)
+        # setup plt
+        fig_w = max(fig_w_ratio * len(hierarchy), 8)
+        fig_h = fig_h_ratio * max(map(len, hierarchy))
+        plt.figure(figsize=(fig_w, fig_h), dpi=dpi)
+        box = plt.gca().get_position()
+        plt.gca().set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        # map key to indices
+        key2idx = {}
+        for layer in hierarchy:
+            for node in layer:
+                key2idx[node.key] = len(key2idx)
+        # add nodes
+        for i, layer in enumerate(hierarchy):
+            for node in layer:
+                G.add_node(key2idx[node.key], subset=f"layer_{i}")
+        # add edges
+        for dep, links in in_edges.items():
+            for link in links:
+                label = edge_labels[(link, dep)]
+                G.add_edge(key2idx[dep], key2idx[link], label=label)
+        # calculate positions
+        layout_fn = getattr(nx, layout, None)
+        if layout_fn is None:
+            raise ValueError(f"unknown layout: {layout}")
+        pos = layout_fn(G)
+        # draw the nodes
+        nodes_styles = dict(
+            node_size=node_size,
+            node_shape=node_shape,
+            node_color=node_color,
+        )
+        nx.draw_networkx_nodes(G, pos, **nodes_styles)
+        node_labels_styles = dict(
+            font_size=18,
+        )
+        nx.draw_networkx_labels(G, pos, **node_labels_styles)
+        # draw the edges
+        nx_edge_labels = nx.get_edge_attributes(G, "label")
+        nx.draw_networkx_edges(
+            G,
+            pos,
+            arrows=True,
+            arrowstyle="-|>",
+            arrowsize=16,
+            node_size=nodes_styles["node_size"],
+            node_shape=nodes_styles["node_shape"],
+        )
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=nx_edge_labels)
+        # draw captions
+        patches = [
+            mpatches.Patch(color=node_color, label=f"{idx}: {key}")
+            for key, idx in key2idx.items()
+        ]
+        plt.legend(handles=patches, bbox_to_anchor=(1, 0.5), loc="center left")
+        # render
+        plt.axis("off")
+        buf = io.BytesIO()
+        plt.savefig(buf, format="png")
+        buf.seek(0)
+        return Image.open(buf)
+
+    def to_json(self) -> List[Dict[str, Any]]:
+        return [node.data.dict() for node in self]
+
+    @classmethod
+    def from_json(cls, data: List[Dict[str, Any]]) -> "Workflow":
+        workflow = cls()
+        for json in data:
+            workflow.push(WorkNode(**json))
+        return workflow
+
+
+__all__ = [
+    "InjectionPack",
+    "WorkNode",
+    "ToposortResult",
+    "Workflow",
+]
