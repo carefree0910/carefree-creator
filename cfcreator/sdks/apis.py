206f2350de6259652338bb297b2e7bb7dbaef451
carefree0910 <syameimaru.saki@gmail.com>
2023-06-13 16:34:54 +0800

    ✨Supported `resize` in `APIs`

--- cfcreator/sdks/apis.py
+++ cfcreator/sdks/apis.py
@@ -39,6 +39,7 @@ endpoint2method = {
     img2img_harmonization_endpoint: "harmonization",
     paste_pipeline_endpoint: "paste_pipeline",
     cv_affine_endpoint: "affine",
+    cv_resize_endpoint: "resize",
     txt2txt_prompt_enhance_endpoint: "prompt_enhance",
     UPLOAD_ENDPOINT: "get_image",
     CONTROL_HINT_ENDPOINT: "get_control_hint",
@@ -141,6 +142,9 @@ class APIs:
 
     async def affine(self, data: AffineModel, **kw: Any) -> List[Image.Image]:
         return await self._run(data, cv_affine_endpoint, **kw)
+
+    async def resize(self, data: ResizeModel, **kw: Any) -> List[Image.Image]:
+        return await self._run(data, cv_resize_endpoint, **kw)
 
     async def prompt_enhance(self, data: PromptEnhanceModel, **kw: Any) -> List[str]:
         task = endpoint2algorithm(txt2txt_prompt_enhance_endpoint)
